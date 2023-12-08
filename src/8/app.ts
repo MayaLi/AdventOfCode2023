3a5b4c46dbc9827a988993db46e5c81624d12443
Maya Li <maya.hao.li@gmail.com>
2023-12-08 11:05:54 -0800

    day 8

---
+++ src/8/app.ts
@@ -0,0 +1,113 @@
+import { Network } from './types'
+import { PART_1_NETWORK, PART_1_INSTRUCTIONS } from './part_1_input'
+import {
+    PART_2_INSTRUCTIONS_EXAMPLE,
+    PART_2_NETWORK_EXAMPLE,
+    PART_2_NETWORK,
+    PART_2_INSTRUCTIONS,
+} from './part_2_input'
+import { boolean } from 'yargs'
+
+/**
+ * Find the number of steps that it takes to navigate from AAA to ZZZ in the network
+ * following provided instructions
+ *
+ * @param network Network
+ * @param instructions string
+ * @returns number steps
+ */
+function part1(network: Network, instructions: string): number {
+    const START = 'AAA'
+    const TARGET = 'ZZZ'
+
+    const instructionLength = instructions.length
+    let numSteps = 0
+    let curr = START
+
+    while (curr !== TARGET) {
+        console.log({ numSteps, curr })
+        const currInstruction = instructions.charAt(
+            numSteps % instructionLength
+        )
+        const nextStepIdx = directionIndex(currInstruction)
+        curr = network[curr][nextStepIdx]
+        numSteps++
+    }
+
+    return numSteps
+}
+
+function directionIndex(direction: string): number {
+    if (direction === 'L') return 0
+    else if (direction === 'R') return 1
+    else throw new Error('unknown direction: ' + direction)
+}
+
+/**
+ * Return the number of steps it takes to start with all nodes that end in "A", following instructions for nodes
+ * and end up in "Z".
+ *
+ * @param network Network
+ * @param instructions string
+ * @returns number
+ */
+function part2(
+    network: Network,
+    instructions: string,
+    numSteps: number = 0,
+    startNodes: string[] = []
+): number {
+    const instructionLength = instructions.length
+    let currNodes =
+        startNodes.length === 0 ? getPart2StarterNodes(network) : startNodes
+    const nodeCount = currNodes.length
+
+    while (true) {
+        const numReachedZ = numNodesReachedTarget(currNodes, nodeCount)
+        if (numReachedZ === nodeCount) {
+            break
+        } else if (numReachedZ >= 3) {
+            console.log({ numReachedZ, numSteps, currNodes })
+        }
+
+        const currInstruction = instructions.charAt(
+            numSteps % instructionLength
+        )
+        const nextStepIdx = directionIndex(currInstruction)
+        currNodes = currNodes.map((n) => {
+            return network[n][nextStepIdx]
+        })
+
+        numSteps++
+    }
+    console.log('Finished!!', { numSteps, currNodes, nodeCount })
+    return numSteps
+}
+
+function getPart2StarterNodes(network: Network): string[] {
+    return Object.keys(network).filter((n) => n.charAt(2) === 'A')
+}
+
+function numNodesReachedTarget(nodes: string[], nodeCount: number): number {
+    return nodes.filter((n) => n.charAt(2) === 'Z').length
+}
+
+export function runPart1() {
+    console.log('part 1 start')
+    const steps1 = part1(PART_1_NETWORK, PART_1_INSTRUCTIONS)
+    console.log(steps1)
+}
+
+export function runPart2() {
+    console.log('part 2 start')
+    // const steps = part2(PART_2_NETWORK, PART_2_INSTRUCTIONS)
+    const steps = part2(PART_2_NETWORK, PART_2_INSTRUCTIONS, 8698533078, [
+        'PQZ',
+        'VDX',
+        'DCV',
+        'JVQ',
+        'BGX',
+        'GKL',
+    ])
+    console.log(steps)
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello, World!");
+import yargs from 'yargs';
+import {runPart1 as day8part1, runPart2 as day8part2} from './8/app';
+
+const args = yargs.options({
+  'day': { type: 'number', demandOption: true, alias: 'd' },
+  'part': { type: 'number', default: 0, alias: 'p' },
+}).parseSync();
+
+const day = args.day;
+const part = args.part;
+
+console.log("Day " + day)
+switch (day) {
+    case 8:
+        if (part === 1) { 
+            day8part1();
+            break;
+         } else if (part === 2) {
+            day8part2() 
+            break;
+         }
+    default: 
+        console.log("Not implemented"); 
+        break;
+}
